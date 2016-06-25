622420239369962569222e08384ef2446914002b
caioferraresi <caioferraresi@hotmail.com>
2016-06-25 15:43:40 -0300

    Alterações na tela de urna

--- src/model/Tela.java
+++ src/model/Tela.java
@@ -18,6 +18,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.SystemColor;
 import javax.swing.JLabel;
+import javax.swing.JTextPane;
 
 public class Tela extends JFrame {
 	//string que contém o id do candidato e deverá ser enviada para o servidor
@@ -48,96 +49,109 @@ public class Tela extends JFrame {
 		
 		setTitle("Urna Eletr\u00F4nica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 621, 400);
+		setBounds(100, 100, 700, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btn1 = new JButton("1");
-		btn1.setBounds(357, 44, 49, 47);
+		btn1.setBounds(449, 44, 49, 47);
 		contentPane.add(btn1);
 		
 		JButton btn2 = new JButton("2");
-		btn2.setBounds(416, 44, 49, 47);
+		btn2.setBounds(508, 44, 49, 47);
 		contentPane.add(btn2);
 		
 		JButton btn3 = new JButton("3");
-		btn3.setBounds(475, 44, 49, 47);
+		btn3.setBounds(567, 44, 49, 47);
 		contentPane.add(btn3);
 		
 		JButton btn4 = new JButton("4");
-		btn4.setBounds(357, 102, 49, 47);
+		btn4.setBounds(449, 102, 49, 47);
 		contentPane.add(btn4);
 		
 		JButton btn5 = new JButton("5");
-		btn5.setBounds(416, 102, 49, 47);
+		btn5.setBounds(508, 102, 49, 47);
 		contentPane.add(btn5);
 		
 		JButton btn6 = new JButton("6");
-		btn6.setBounds(475, 102, 49, 47);
+		btn6.setBounds(567, 102, 49, 47);
 		contentPane.add(btn6);
 		
 		JButton btn7 = new JButton("7");
-		btn7.setBounds(357, 160, 49, 47);
+		btn7.setBounds(449, 160, 49, 47);
 		contentPane.add(btn7);
 		
 		JButton btn8 = new JButton("8");
-		btn8.setBounds(416, 160, 49, 47);
+		btn8.setBounds(508, 160, 49, 47);
 		contentPane.add(btn8);
 		
 		JButton btn9 = new JButton("9");
-		btn9.setBounds(475, 160, 49, 47);
+		btn9.setBounds(567, 160, 49, 47);
 		contentPane.add(btn9);
 		
 		JButton btn0 = new JButton("0");
-		btn0.setBounds(416, 218, 49, 47);
+		btn0.setBounds(508, 218, 49, 47);
 		contentPane.add(btn0);
 		
 		JButton btnBranco = new JButton("Branco");
 		btnBranco.setBackground(Color.WHITE);
-		btnBranco.setBounds(315, 294, 80, 29);
+		btnBranco.setBounds(407, 294, 80, 29);
 		contentPane.add(btnBranco);
 		
 		JButton btnCorrige = new JButton("Corrige");
 		btnCorrige.setBackground(SystemColor.desktop);
-		btnCorrige.setBounds(405, 294, 80, 29);
+		btnCorrige.setBounds(497, 294, 80, 29);
 		contentPane.add(btnCorrige);
 		
 		JButton btnConfirma = new JButton("Confirma");
 		btnConfirma.setBackground(new Color(34, 139, 34));
-		btnConfirma.setBounds(494, 276, 88, 47);
+		btnConfirma.setBounds(586, 276, 88, 47);
 		contentPane.add(btnConfirma);
 		
 		TextField txtNomeCandidato = new TextField();
 		txtNomeCandidato.setEditable(false);
-		txtNomeCandidato.setBounds(22, 142, 228, 22);
+		txtNomeCandidato.setBounds(226, 142, 174, 22);
 		contentPane.add(txtNomeCandidato);
 		
 		Label label = new Label("Canditado");
-		label.setBounds(22, 114, 62, 22);
+		label.setBounds(226, 114, 62, 22);
 		contentPane.add(label);
 		
 		Label label_1 = new Label("N\u00FAmero");
-		label_1.setBounds(22, 44, 62, 22);
+		label_1.setBounds(226, 44, 62, 22);
 		contentPane.add(label_1);
 		
 		TextField txtIDCandidato = new TextField();
 		txtIDCandidato.setEditable(false);
-		txtIDCandidato.setBounds(22, 72, 228, 22);
+		txtIDCandidato.setBounds(226, 72, 174, 22);
 		contentPane.add(txtIDCandidato);
 		
 		JLabel lblAperte = new JLabel("Aperte ");
-		lblAperte.setBounds(22, 274, 46, 14);
+		lblAperte.setBounds(226, 276, 46, 14);
 		contentPane.add(lblAperte);
 		
 		JLabel lblVerdeParaConfirmar = new JLabel("VERDE para CONFIRMAR");
-		lblVerdeParaConfirmar.setBounds(22, 293, 155, 14);
+		lblVerdeParaConfirmar.setBounds(226, 295, 155, 14);
 		contentPane.add(lblVerdeParaConfirmar);
 		
 		JLabel lblLaranjaParaCorrigir = new JLabel("LARANJA para CORRIGIR");
-		lblLaranjaParaCorrigir.setBounds(22, 309, 155, 14);
+		lblLaranjaParaCorrigir.setBounds(226, 311, 155, 14);
 		contentPane.add(lblLaranjaParaCorrigir);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBounds(10, 72, 174, 251);
+		contentPane.add(textPane);
+		
+		JButton btnListarCandidatos = new JButton("Listar Candidatos");
+		btnListarCandidatos.setBounds(10, 43, 174, 23);
+		contentPane.add(btnListarCandidatos);
+		
+		JButton btnFinalizarVotao = new JButton("Finalizar Vota\u00E7\u00E3o");
+		btnFinalizarVotao.setBounds(497, 395, 177, 55);
+		contentPane.add(btnFinalizarVotao);
 		
 		btn1.addMouseListener(new MouseAdapter() {
 			@Override
