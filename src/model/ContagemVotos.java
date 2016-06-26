b88bdef787f8c8ad46c77ae5df405a623f9497d6
caioferraresi <caioferraresi@hotmail.com>
2016-06-26 14:57:56 -0300

    Tela Contagem de Votos - upgrade

--- src/model/ContagemVotos.java
+++ src/model/ContagemVotos.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
 
 public class ContagemVotos extends JFrame {
 
@@ -35,8 +37,16 @@ public class ContagemVotos extends JFrame {
 		setBounds(100, 100, 700,500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblResultadoDaVotao = new JLabel("Resultado da Vota\u00E7\u00E3o:");
+		lblResultadoDaVotao.setBounds(26, 11, 227, 14);
+		contentPane.add(lblResultadoDaVotao);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBounds(26, 36, 637, 398);
+		contentPane.add(textPane);
 	}
-
 }
