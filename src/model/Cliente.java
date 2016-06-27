bb1a90b95a61f8e371098416839bf5ad254a1f03
thomas-ms <thomas.ms@usp.br>
2016-06-27 03:34:34 -0300

    Mudanca na clase Cliente: criacao dos metodos enviarVotos e receberCandidatos

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,14 +1,15 @@
 package model;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.Socket;	
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;	
 
 public class Cliente {
 	
@@ -42,5 +43,51 @@ public class Cliente {
         } catch (Exception e) {
             System.out.println("Erro 6: " + e.getMessage());
         }
-	}	
+	}
+	
+	public void enviaVotos(ArrayList<Candidato> candidatos){
+		try {
+            File arq = new File("Candidatos.dat");
+            arq.createNewFile();
+            FileOutputStream canoOut = new FileOutputStream(arq);
+            ObjectOutputStream serializador = new ObjectOutputStream(canoOut);
+            serializador.writeObject(candidatos);
+            serializador.flush();
+            serializador.close();
+            canoOut.close();
+            
+            dadosAEnviar = new ObjectOutputStream(socketCliente.getOutputStream());
+            FileInputStream file = new FileInputStream("Candidatos.dat"); 
+            
+            byte[] buf = new byte[4062];
+            int i = 0;
+            while(true){
+                int len = file.read(buf);
+                if(len == -1) break;
+                dadosAEnviar.write(buf, i, len);
+                i++;
+            }
+            dadosAEnviar.flush();
+            file.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}
+	
+	public ArrayList<Candidato> recebeCandidatos(){
+		ArrayList<Candidato> candidatos = new ArrayList<>();
+        try {
+            File arq = new File("Candidatos.dat");
+            FileInputStream canoIn = new FileInputStream(arq);
+            ObjectInputStream serializador = new ObjectInputStream(canoIn);
+            candidatos = (ArrayList<Candidato>) serializador.readObject();
+            serializador.close();
+            canoIn.close();            
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return candidatos;
+    }
 }
