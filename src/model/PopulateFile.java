6b0accc32bd7f9c65ba3686ac7c7b3e019e6b51a
grupo05bsi grupo05bsi <grupo05bsi@node10.clasdpc.lan>
2016-06-28 17:48:27 -0300

    Inserir Candidatos

--- src/model/PopulateFile.java
+++ src/model/PopulateFile.java
@@ -11,14 +11,14 @@ public class PopulateFile{
 	public static void main(String args[]) throws IOException{
 		ArrayList<Candidato> candidatos = new ArrayList<Candidato>();
 		
-		candidatos.add(new Candidato(1111,"Caio","PQP",0));
-		candidatos.add(new Candidato(1112,"Caiolino","GAP",0));
-		candidatos.add(new Candidato(1113,"Caiuu","GOT",0));
-		candidatos.add(new Candidato(0000,"Branco","S/P",0));
+		candidatos.add(new Candidato(1111,"Caio","PT",0));
+		candidatos.add(new Candidato(2222,"Sabrina","PSDB",0));
+		candidatos.add(new Candidato(3333,"Thomás","PSOL",0));
+		candidatos.add(new Candidato(0000,"Branco","",0));
 
-		File arq = new File("CandidatosServidor.dat");
+		File arq = new File("/home/grupo05bsi/CandidatosServidor.dat");
     	arq.createNewFile();
-		FileOutputStream canoOut = new FileOutputStream("CandidatosServidor.dat");
+		FileOutputStream canoOut = new FileOutputStream("/home/grupo05bsi/CandidatosServidor.dat");
 		ObjectOutputStream serializador = new ObjectOutputStream(canoOut);
 		serializador.writeObject(candidatos);
 		serializador.flush();
